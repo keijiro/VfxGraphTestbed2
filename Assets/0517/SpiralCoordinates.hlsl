283563c23de11b890feec04b0a755f0a02ab9688
Keijiro Takahashi <keijiro@gmail.com>
2021-05-17 17:47:50 +0900

    Tweaks

--- Assets/0517/SpiralCoordinates.hlsl
+++
@@ -1,6 +0,0 @@
-void SpiralCoordinates_float(float3 pos, float freq, out float2 coord)
-{
-    const float omega = 6.28318530718;
-    float phi = atan2(pos.y, pos.x) / omega + 0.5;
-    coord = float2(phi, pos.z - phi * freq);
-}
